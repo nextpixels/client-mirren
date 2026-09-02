dfeb01b56dbf3a1d3ad7987c524129958fde6dfb
nextpixels <forrest@nextpixels.com>
2026-09-02 08:12:53 -0700

    Hero Footer CTA - Element alignment

--- elements-acf/hero-footer-cta/hero-footer-cta.php
+++ elements-acf/hero-footer-cta/hero-footer-cta.php
@@ -26,18 +26,24 @@
 ?>
 
 <section id="hero-footer-cta" class="p-t-25 p-b-25">
-	<div class="contain-1200 p-mobile-1250 text-center">
-		<h2><?php echo $title; ?></h2><?php
-		if ($text){ ?>
-			<p><?php echo $text; ?></p><?php
-		} ?>
-		<div class="p-t-15"><?php
-			if (stristr($buttonTarget,"http")){ ?>
-				<a href="<?php echo $buttonTarget; ?>" class="btn btn-primary"><?php echo $buttonLabel; ?></a><?php
-			}
-			else{ ?>
-				<a href="<?php echo get_site_url()."/".$buttonTarget; ?>" class="btn btn-primary"><?php echo $buttonLabel; ?></a><?php
-			}	?>
+	<div class="contain-1200 p-mobile-1250">
+
+		<div class="hero-footer-cta-inner">
+			<div class="hero-footer-cta-button"><?php
+				if (stristr($buttonTarget,"http")){ ?>
+					<a href="<?php echo $buttonTarget; ?>" class="btn btn-primary"><?php echo $buttonLabel; ?></a><?php
+				}
+				else{ ?>
+					<a href="<?php echo get_site_url()."/".$buttonTarget; ?>" class="btn btn-primary"><?php echo $buttonLabel; ?></a><?php
+				}	?>
+			</div>
+			<div class="hero-footer-cta-text">
+				<h2><?php echo $title; ?></h2><?php
+				if ($text){ ?>
+					<p><?php echo $text; ?></p><?php
+				} ?>
+			</div>
 		</div>
+
 	</div>
 </section>
